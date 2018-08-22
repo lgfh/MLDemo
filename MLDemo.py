b67951144a78f25dd2e640219b8ba2d4983b7713
lgfh <wxy11254018@gmail.com>
2018-08-23 00:45:30 +0800

    commit two simple ML demo

---
+++ MLDemo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from sklearn import tree
+
+features = [[140,1],[130,1],[150,1],[170,0]]
+lables = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features,lables)
+print clf.predict([[160,0]])
---
+++ viz.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+#print iris.feature_names
+#print iris.target_names
+#print iris.data[0]
+#print iris.target[0]
+for i in range(len(iris.target)):
+    print "Example %d: lable %s , feature %s " %(i,iris.target[i],iris.data[i])
